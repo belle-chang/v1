1f39c5eb9c67f6a0a70332da2383238673df51c2
Anabelle <bellech88@gmail.com>
2019-08-06 21:21:54 -0700

    configuring contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,30 @@
 import React from "react"
+import { navigate } from 'gatsby-link'
 import Layout from "../components/layout"
 import { Helmet } from "react-helmet"
 
 function Contact() {
+    const [state, setState] = React.useState({})
+  
+    const handleChange = (e) => {
+      setState({ ...state, [e.target.name]: e.target.value })
+    }
+  
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      const form = e.target
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({
+          'form-name': form.getAttribute('name'),
+          ...state,
+        }),
+      })
+        .then(() => navigate(form.getAttribute('action')))
+        .catch((error) => alert(error))
+    }
+  
     return (
         <Layout>
             <Helmet>
@@ -11,49 +33,48 @@ function Contact() {
             </Helmet>
             <h1>Contact me below!</h1>
 
-            {/* <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form> */}
-            <form 
-                form-name="contact_me"
-                method="post"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-            >
-                <label for="name">Your name:**</label>
-                <br />
-                <input name="name" type="text" required="true" />
-                <br />
-                <label for="email">Your email:**</label>
-                <br />
-                <input name="email" type="text" required="true" />
-                <br />
-                <label for="message">Your message:**</label>
-                <br />
-                <input name="message" type="textarea" required="true" />
-                <br />
-                <button type="submit">Send!</button>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+            </label>
+          </p>
 
-            </form>
-        </Layout>
+          <p>
+            <label>
+              Your name:
+              <br />
+              <input type="text" name="name" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:
+              <br />
+              <input type="email" name="email" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:
+              <br />
+              <textarea name="message" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </Layout>
     )
-}
-
+  }
 export default Contact
