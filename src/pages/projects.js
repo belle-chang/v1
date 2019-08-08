99d09264fec5bb0a204e91b992cdeffb6c6202a7
Anabelle <bellech88@gmail.com>
2019-08-08 01:50:30 -0700

    clickable links with order of importance

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,38 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
-import { graphql } from 'gatsby'
-
-export const query = `
-    query (
-        $slug: String!
-    ) {
-        markdownRemark(
-        fields:{
-            slug:{
-            eq: $slug
-            }
-        }
-        ) {
-        frontmatter {
-            title
-            date
-            image
-            tile
-            description
-            key
-        }
-        html
-        }
-    }
-`
 function Projects(props) {
     return (
         <Layout>
-            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-            <p>{props.data.markdownRemark.frontmatter.date}</p>
-            <div dangerouslySetInnerHTML={{
-                __html: props.data.markdownRemark.html
-            }}></div>
         </Layout>
     )
 }
