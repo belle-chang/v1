b6b5ba10a88d654bf5fe62e0d412315cef2e02ea
Anabelle <bellech88@gmail.com>
2019-08-08 21:35:06 -0700

    added tooltip

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -24,18 +24,30 @@ function NavBar() {
             <li ><Link to="/about" className={navBarStyles.reg}>about</Link></li>
             <li ><Link to="/contact" className={navBarStyles.reg}>contact me</Link></li>
             <li>
-                <a href="https://github.com/belle-chang"
-                    target="_blank"
-                    className={navBarStyles.reg}
-                    style={{fontSize: `25px`}}>
-                        <FontAwesomeIcon icon={faGithub}/>
-                </a>
-                <a href="https://www.linkedin.com/in/anabelle-chang-620362102"
-                    target="_blank"
-                    className={navBarStyles.reg}
-                    style={{fontSize: `25px`}}>
-                        <FontAwesomeIcon icon={faLinkedin}/>
-                </a>
+                <div className={navBarStyles.tooltip}>
+                    <a href="https://github.com/belle-chang"
+                            target="_blank"
+                            className={navBarStyles.reg}
+                            style={{fontSize: `25px`, textAlign: `center`}}>
+                                <FontAwesomeIcon icon={faGithub}/>
+                        </a>
+                    <span className={navBarStyles.tooltiptext}>
+                        check out my latest work!
+                    </span>
+                </div>                
+            </li>
+            <li>
+            <div className={navBarStyles.tooltip}>
+                    <a href="https://www.linkedin.com/in/anabelle-chang-620362102"
+                        target="_blank"
+                        className={navBarStyles.reg}
+                        style={{fontSize: `25px`}}>
+                            <FontAwesomeIcon icon={faLinkedin}/>
+                    </a>
+                    <span className={navBarStyles.tooltiptext}>
+                        connect with me
+                    </span>
+                </div>
             </li>
             
             </ul>
